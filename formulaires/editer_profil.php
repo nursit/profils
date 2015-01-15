007d0babf128dc0e24862ef84b32fcffa4779da0
Cerdic <cedric@yterium.com>
2015-01-15 14:31:15 +0100

    robustesse du test

--- formulaires/editer_profil.php
+++ formulaires/editer_profil.php
@@ -12,22 +12,23 @@ include_spip("base/abstract_sql");
 
 function formulaires_editer_profil_charger_dist($id_auteur){
 	if (!$id_auteur
-	  OR !$auteur = sql_fetsel('*','spip_auteurs','id_auteur='.intval($id_auteur)))
+		OR !$auteur = sql_fetsel('*', 'spip_auteurs', 'id_auteur=' . intval($id_auteur))
+	)
 		return false;
 
 
 	$valeurs = array('newsletter' => '');
-	foreach(array('nom',
-			'name',
-	              'prenom',
-	              'email',
-	              'societe',
-	              'adresse',
-	              'adresse_cp',
-	              'adresse_ville',
-	              'adresse_pays',
-	              'tel_fixe',
-	              'tel_mobile') as $champ)
+	foreach (array('nom',
+		         'name',
+		         'prenom',
+		         'email',
+		         'societe',
+		         'adresse',
+		         'adresse_cp',
+		         'adresse_ville',
+		         'adresse_pays',
+		         'tel_fixe',
+		         'tel_mobile') as $champ)
 		$valeurs[$champ] = $auteur[$champ];
 
 	// abonne a la newsletter ?
@@ -49,17 +50,17 @@ function formulaires_editer_profil_verifier_dist($id_auteur){
 	$erreurs = array();
 
 	$oblis = array('name',
-	              'prenom',
-	              'email',
-	              #'adresse',
-	              #'adresse_cp',
-	              #'adresse_ville',
-	              #'adresse_pays'
+		'prenom',
+		'email',
+		#'adresse',
+		#'adresse_cp',
+		#'adresse_ville',
+		#'adresse_pays'
 	);
 
-	foreach($oblis as $obli)
+	foreach ($oblis as $obli)
 		if (!strlen(_request($obli)))
-			$erreurs[$obli] = _T('editer_profil:erreur_'.$obli.'_obligatoire');
+			$erreurs[$obli] = _T('editer_profil:erreur_' . $obli . '_obligatoire');
 
 	// Verifier l'email
 	if (!isset($erreurs['email'])){
@@ -71,54 +72,59 @@ function formulaires_editer_profil_verifier_dist($id_auteur){
 		}
 	}
 
-    /* 15/09/2014  Pour le pseudo, on veut bien qu'au cas où le compte auteur existe, et
-        des articles y sont associés, que la modification du pseudo dans
-        l'espace lecteur soit interdite. */
+	/* On ne permet pas aux redacteurs/admin de modifier leur pseudo ici
+	 si ils ont des articles publies car cela impacte leur signature */
 
-    $nb_articles = sql_countsel('spip_auteurs_liens', 'id_auteur=' . intval($id_auteur) . " and objet='article'");
-    if ($nb_articles > 0) {
-        $nom_auteur = sql_fetsel('nom', 'spip_auteurs', 'id_auteur=' . intval($id_auteur));
-        if (_request('nom')
-            AND _request('nom') !== $nom_auteur
-        ) {
-            $erreurs['nom'] = _T('editer_profil:erreur_impossible_modifier_pseudo_auteur');
-        }
+	// 0minirezo ou 1comite
+	if (intval($GLOBALS['visiteur_session']['statut'])<=1){
 
-    }
-    
+		$nb_articles = sql_countsel('spip_auteurs_liens AS L JOIN spip_articles as A ON (A.id_article=L.id_objet AND L.objet='.sql_quote('article').')', 'L.id_auteur=' . intval($id_auteur) . " AND L.objet='article' AND A.statut=".sql_quote('publie'));
+		if ($nb_articles>0){
+			$nom_auteur = sql_fetsel('nom', 'spip_auteurs', 'id_auteur=' . intval($id_auteur));
+			if (_request('nom')
+				AND _request('nom')!==$nom_auteur
+			){
+				$erreurs['nom'] = _T('editer_profil:erreur_impossible_modifier_pseudo_auteur');
+			}
+
+		}
+	}
+
 	return $erreurs;
 }
 
 function formulaires_editer_profil_traiter_dist($id_auteur){
 
 	refuser_traiter_formulaire_ajax();
-	$auteur = sql_fetsel('*','spip_auteurs','id_auteur='.intval($id_auteur));
+	$auteur = sql_fetsel('*', 'spip_auteurs', 'id_auteur=' . intval($id_auteur));
 
 	// si l'email change
 	if (_request('email')
-	  AND _request('email')!==$auteur['email']){
+		AND _request('email')!==$auteur['email']
+	){
 
 		// si c'�tait le login, changer aussi le login
 		if ($auteur['email']==$auteur['login'])
-			set_request('login',_request('email'));
+			set_request('login', _request('email'));
 
 	}
 
 	include_spip('inc/editer');
 	// renseigner le nom de la table auteurs (s'il n'a pas été renseigné)
-    if (!_request('nom'))
-        set_request('nom',_request('prenom').' '._request('name'));
-	$res = formulaires_editer_objet_traiter('auteur',$id_auteur);
+	if (!_request('nom'))
+		set_request('nom', _request('prenom') . ' ' . _request('name'));
+	$res = formulaires_editer_objet_traiter('auteur', $id_auteur);
 
 	// si l'email change
 	if (_request('email')
-	  AND _request('email')!==$auteur['email']){
+		AND _request('email')!==$auteur['email']
+	){
 
 		// securite si jamais la modif en base n'a pas eu lieu
-		$new_email = sql_getfetsel('email','spip_auteurs','id_auteur='.intval($id_auteur));
+		$new_email = sql_getfetsel('email', 'spip_auteurs', 'id_auteur=' . intval($id_auteur));
 
 		// updater les abonnements dans mailsubscribers
-		sql_updateq("spip_mailsubscribers",array('email'=>$new_email),"email=".sql_quote($auteur['email']));
+		sql_updateq("spip_mailsubscribers", array('email' => $new_email), "email=" . sql_quote($auteur['email']));
 	}
 
 	/*
